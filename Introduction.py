1ef0c205bb4538f8f1b49eb541241d04261349a0
Pravflix <pranovjpk26@gmail.com>
2023-12-23 12:10:23 +0000

    Introductory

---
+++ Introduction.py
@@ -0,0 +1,29 @@
+# Welcome To Python
+
+# This is going to be an introductory for the programming language Python
+
+# It'll compromise everything you would need to know about Python and how to tackle it
+
+# Let's start! Hope you have fun :)
+
+# Before we start if you're completely new to this then i would recommend using Pycharm as your IDE
+# IDE = Integrated Development Environment (A software used to create other software!)
+
+# You should also download the python software to make this work!
+
+# Now let's actually start!
+
+# By the way if you would like the videos I learnt this from i have attached it on my github
+# (I guess you're looking at this from there!)
+
+print("Hello!My friend")
+
+print("This is going to be a wonderful journey Let's Start Learning! :0 ")
+
+
+# Lesson 1
+
+# What's a variable? Mate Explain...
+
+
+
